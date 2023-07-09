dfffa6e6a632f5c1258ccbf23a4da6ec14ae99ad
Sharathn002 <125349872+Sharathn002@users.noreply.github.com>
2023-07-09 21:53:47 +0530

    Update generate_output.py

--- generate_output.py
+++ generate_output.py
@@ -1,1 +1,13 @@
-print("hello")
+import argparse
+from datetime import datetime, timedelta
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--TIMEOUT', help='duration of the silence', default='')
+    args = parser.parse_args()
+    duration=float(args.TIMEOUT.lower().split('h')[0])
+    nine_hours_from_now = datetime.now() + timedelta(hours=duration)
+    print(nine_hours_from_now)
+
+if __name__=='__main__':
+    main()
